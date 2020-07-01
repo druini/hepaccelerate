a92692416a6bf9ef2f540be1ed797e4f20efd392
Daniele Ruini <daniele.ruini@cern.ch>
2020-07-01 09:54:18 +0200

    try statement for the case some plots are not present

--- rhalphabet/makePlots.py
+++ rhalphabet/makePlots.py
@@ -68,7 +68,8 @@ def plotRhalphaShapes(rootFilePath, nptbins):
     outdir = os.getcwd()
   else:
     outdir = os.path.join(*rootFilePath.split(os.path.sep)[:-1])
-  outdir = os.path.join(outdir,'plots')
+  #outdir = os.path.join(outdir,'plots')
+  outdir = os.path.join(outdir,'plots'+os.path.splitext(rootFilePath.split('fitDiagnostics')[1])[0])
   if not os.path.exists(outdir):
     os.mkdir(outdir)
 
@@ -100,84 +101,86 @@ def plotRhalphaShapes(rootFilePath, nptbins):
   for ibin in range(nptbins):
     for region in ['Pass','Fail']:
       for SorB in ['s','b']:
-        p = f'ptbin{ibin}{region}_msd_fit_{SorB}'
-        can = rt.TCanvas('c2', 'c1',  10, 10, 750, 750 )
-        pad1 = rt.TPad("pad1", "Fit",0,0.207,1.00,1.00,-1)
-        pad2 = rt.TPad("pad2", "Pull",0,0.00,1.00,0.28,-1)
-        pad1.Draw()
-        pad2.Draw()
-        pad1.cd()
-
-        leg = rt.TLegend(0.70,0.7,0.90,0.87)
-        leg.SetFillStyle(0)
-        leg.SetTextSize(0.04)
-        plot   = rootfile.Get(p)
-        b_suffix = '_model_bonly__' if SorB=='b' else ''
-        RooObj = {
-          'data'       : plot.getHist(f'h_ptbin{ibin}{region}'),
-          'errs'       : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_errorband'),
-          'signal'     : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeSig*]'),
-          'bkg'        : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeBkg*]'),
-          'sigPlusBkg' : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]')
-        }
-
-        rmin = RooObj['data'].GetXaxis().GetXmin()
-        rmax = RooObj['data'].GetXaxis().GetXmax()
-        xmin = (rmax+rmin)/2 - (rmax-rmin)/2.4
-        xmax = (rmax+rmin)/2 + (rmax-rmin)/2.4
-        RooObj['errs'].GetYaxis().SetTitle( f'Events / {RooObj["data"].getNominalBinWidth()} GeV' )
-        #RooObj['errs'].GetYaxis().SetTitleOffset( 0.5 )
-        #RooObj['errs'].GetYaxis().SetLabelSize(0.12)
-        #RooObj['errs'].GetYaxis().SetTitleSize(0.12)
-        for s in ['errs', 'signal', 'bkg', 'sigPlusBkg', 'data']:
-          RooObj[s].GetXaxis().SetLabelOffset(999)
-          RooObj[s].GetXaxis().SetLabelSize(0)
-          RooObj[s].GetXaxis().SetLimits(xmin,xmax)
-          RooObj[s].Draw(drawOpts[s])
-          if s in leglab:
-            leg.AddEntry( RooObj[s], leglab[s], legopts[s] )
-
-        CMS_lumi.cmsTextOffset = 0.0
-        CMS_lumi.relPosX = 0.13
-        CMS_lumi.CMS_lumi(pad1, 4, 0)
-        leg.Draw()
-    #    #ymax = h['data_obs'].GetMaximum() + 1.5*sqrt(h['data_obs'].GetMaximum())
-    #    #h[namesInRoot[0]].GetYaxis().SetRangeUser(0, ymax)
-
-
-        hRatio = rt.TGraphAsymmErrors()
-        data_h = TH1fromRooObj(RooObj['data'])
-        sigPlusBkg_h = TH1fromRooObj(RooObj['sigPlusBkg'],RooObj['errs'])
-        hRatio.Divide( data_h,sigPlusBkg_h, 'pois' )
-        hRatioStatErr = sigPlusBkg_h.Clone()
-        hRatioStatErr.Divide( sigPlusBkg_h )
-        hRatioStatErr.SetFillColor(rt.kBlack)
-        hRatioStatErr.SetFillStyle(3004)
-
-        pad2.cd()
-        rt.gStyle.SetOptFit(1)
-        pad2.SetGrid()
-        pad2.SetTopMargin(0)
-        pad2.SetBottomMargin(0.3)
-        tmpPad2= pad2.DrawFrame(xmin, 0.5, xmax,1.5)
-        tmpPad2.GetXaxis().SetTitle( 'softdrop mass [GeV]' )
-        tmpPad2.GetYaxis().SetTitle( "Data/Bkg" )
-        tmpPad2.GetYaxis().SetTitleOffset( 0.5 )
-        tmpPad2.GetYaxis().CenterTitle()
-        tmpPad2.SetLabelSize(0.12, 'x')
-        tmpPad2.SetTitleSize(0.12, 'x')
-        tmpPad2.SetLabelSize(0.12, 'y')
-        tmpPad2.SetTitleSize(0.12, 'y')
-        tmpPad2.SetNdivisions( data_h.GetNdivisions('x'), 'x')
-        tmpPad2.SetNdivisions(505, 'y')
-        pad2.Modified()
-        hRatio.SetMarkerStyle(8)
-        hRatio.Draw('P')
-        hRatioStatErr.Draw('same e2')
-
-        for ext in ['pdf','png']:
-          can.SaveAs( os.path.join(outdir,f'{p}.{ext}') )
-        del can
+        try:
+          p = f'ptbin{ibin}{region}_msd_fit_{SorB}'
+          can = rt.TCanvas('c2', 'c1',  10, 10, 750, 750 )
+          pad1 = rt.TPad("pad1", "Fit",0,0.207,1.00,1.00,-1)
+          pad2 = rt.TPad("pad2", "Pull",0,0.00,1.00,0.28,-1)
+          pad1.Draw()
+          pad2.Draw()
+          pad1.cd()
+  
+          leg = rt.TLegend(0.70,0.7,0.90,0.87)
+          leg.SetFillStyle(0)
+          leg.SetTextSize(0.04)
+          plot   = rootfile.Get(p)
+          b_suffix = '_model_bonly__' if SorB=='b' else ''
+          RooObj = {
+            'data'       : plot.getHist(f'h_ptbin{ibin}{region}'),
+            'errs'       : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_errorband'),
+            'signal'     : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeSig*]'),
+            'bkg'        : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeBkg*]'),
+            'sigPlusBkg' : plot.getCurve(f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]')
+          }
+  
+          rmin = RooObj['data'].GetXaxis().GetXmin()
+          rmax = RooObj['data'].GetXaxis().GetXmax()
+          xmin = (rmax+rmin)/2 - (rmax-rmin)/2.4
+          xmax = (rmax+rmin)/2 + (rmax-rmin)/2.4
+          RooObj['errs'].GetYaxis().SetTitle( f'Events / {RooObj["data"].getNominalBinWidth()} GeV' )
+          #RooObj['errs'].GetYaxis().SetTitleOffset( 0.5 )
+          #RooObj['errs'].GetYaxis().SetLabelSize(0.12)
+          #RooObj['errs'].GetYaxis().SetTitleSize(0.12)
+          for s in ['errs', 'signal', 'bkg', 'sigPlusBkg', 'data']:
+            RooObj[s].GetXaxis().SetLabelOffset(999)
+            RooObj[s].GetXaxis().SetLabelSize(0)
+            RooObj[s].GetXaxis().SetLimits(xmin,xmax)
+            RooObj[s].Draw(drawOpts[s])
+            if s in leglab:
+              leg.AddEntry( RooObj[s], leglab[s], legopts[s] )
+  
+          CMS_lumi.cmsTextOffset = 0.0
+          CMS_lumi.relPosX = 0.13
+          CMS_lumi.CMS_lumi(pad1, 4, 0)
+          leg.Draw()
+      #    #ymax = h['data_obs'].GetMaximum() + 1.5*sqrt(h['data_obs'].GetMaximum())
+      #    #h[namesInRoot[0]].GetYaxis().SetRangeUser(0, ymax)
+  
+  
+          hRatio = rt.TGraphAsymmErrors()
+          data_h = TH1fromRooObj(RooObj['data'])
+          sigPlusBkg_h = TH1fromRooObj(RooObj['sigPlusBkg'],RooObj['errs'])
+          hRatio.Divide( data_h,sigPlusBkg_h, 'pois' )
+          hRatioStatErr = sigPlusBkg_h.Clone()
+          hRatioStatErr.Divide( sigPlusBkg_h )
+          hRatioStatErr.SetFillColor(rt.kBlack)
+          hRatioStatErr.SetFillStyle(3004)
+  
+          pad2.cd()
+          rt.gStyle.SetOptFit(1)
+          pad2.SetGrid()
+          pad2.SetTopMargin(0)
+          pad2.SetBottomMargin(0.3)
+          tmpPad2= pad2.DrawFrame(xmin, 0.5, xmax,1.5)
+          tmpPad2.GetXaxis().SetTitle( 'softdrop mass [GeV]' )
+          tmpPad2.GetYaxis().SetTitle( "Data/Bkg" )
+          tmpPad2.GetYaxis().SetTitleOffset( 0.5 )
+          tmpPad2.GetYaxis().CenterTitle()
+          tmpPad2.SetLabelSize(0.12, 'x')
+          tmpPad2.SetTitleSize(0.12, 'x')
+          tmpPad2.SetLabelSize(0.12, 'y')
+          tmpPad2.SetTitleSize(0.12, 'y')
+          tmpPad2.SetNdivisions( data_h.GetNdivisions('x'), 'x')
+          tmpPad2.SetNdivisions(505, 'y')
+          pad2.Modified()
+          hRatio.SetMarkerStyle(8)
+          hRatio.Draw('P')
+          hRatioStatErr.Draw('same e2')
+  
+          for ext in ['pdf','png']:
+            can.SaveAs( os.path.join(outdir,f'{p}.{ext}') )
+          del can
+        except: continue
 
 def plotTF(rootFilePath, nptbins):
   if not rootFilePath.endswith(".root"):
@@ -186,7 +189,8 @@ def plotTF(rootFilePath, nptbins):
     outdir = os.getcwd()
   else:
     outdir = os.path.join(*rootFilePath.split(os.path.sep)[:-1])
-  outdir = os.path.join(outdir,'plots')
+  #outdir = os.path.join(outdir,'plots')
+  outdir = os.path.join(outdir,'plots'+os.path.splitext(rootFilePath.split('fitDiagnostics')[1])[0])
   if not os.path.exists(outdir):
     os.mkdir(outdir)
 
