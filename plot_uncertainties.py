2e9df0f7bd6f8ff74c92beab67e1a7dc879b5f38
Daniele Ruini <daniele.ruini@cern.ch>
2020-09-01 18:02:33 +0200

    finish uncertainty plots

--- plot_uncertainties.py
+++ plot_uncertainties.py
@@ -34,16 +34,31 @@ def plot_unc(indir, histName, uncName, outdir):
             'up'      : 'r',
             'down'    : 'b'
             }
+    ls    = {
+            'nominal' : '-',
+            'up'      : '--',
+            'down'    : '-.'
+            }
 
+    rebin_factor = 5
+
+    plt.style.use([hep.cms.style.ROOT, {'font.size': 24}])
     f, ax = plt.subplots()
+    #ax = hep.cms.label(data=False, paper=False, year=args.year, ax=ax, loc=1)
     for hn,h in hist.items():
-        h['edges'], h['contents'], h['contents_w2'] = rebin(h['edges'], h['contents'], h['contents_w2'], 5)
-        hep.histplot(h['contents'], h['edges'], edges=True, color=color[hn])
-    plt.show()
+        h['edges'], h['contents'], h['contents_w2'] = rebin(h['edges'], h['contents'], h['contents_w2'], rebin_factor)
+        hep.histplot(h['contents'], h['edges'], edges=True, label=(hn if hn=='nominal' else f'{uncName} {hn}'),ls=ls[hn],color=color[hn])
+    plt.xlim(90,170)
+    plt.legend()
+    plt.xlabel('Leading AK8 jet softdrop mass [GeV]')
+    plt.ylabel(f'Events / {rebin_factor} GeV')
+    for ext in ['.png','.pdf']:
+         plt.savefig(os.path.join(outdir,f'{args.variable}_{args.mask}_{uncName}{ext}'))
+    #plt.show()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('-u', '--uncertainty', default='jer', help='which uncertainty to plot')
+  #parser.add_argument('-u', '--uncertainty', default='jer', help='which uncertainty to plot')
   parser.add_argument('-r', '--result-dir', default='results')
   parser.add_argument('-y', '--year', default='2017')
   parser.add_argument('-v', '--version')
@@ -56,17 +71,19 @@ if __name__ == '__main__':
   except:
     parser.print_help()
     sys.exit(0)
+    
+  for unc in ['jer','jesTotal','jmr','jms']:
+      if args.path is not None:
+          indir  = args.path
+          outdir = os.path.join('plots', *args.path.split(os.path.sep)[1:])
+      else:
+          indir  = os.path.join(args.result_dir,args.year,args.version,args.selection)
+          outdir = os.path.join('plots',args.year,args.version,args.selection)
+      
+      outdir = os.path.join(outdir,args.mask,'uncertainties')
+      if not os.path.exists(outdir):
+          os.makedirs(outdir)
 
-  if args.path is not None:
-      indir  = args.path
-      outdir = os.path.join('plots', *args.path.split(os.path.sep)[1:])
-  else:
-      indir  = os.path.join(args.result_dir,args.year,args.version,args.selection)
-      outdir = os.path.join('plots',args.year,args.version,args.selection)
-  
-  outdir = os.path.join(outdir,args.uncertainty)
-  if not os.path.exists(outdir):
-      os.makedirs(outdir)
-
-  histName = f'hist_{args.variable}_{args.mask}'
-  plot_unc(indir,histName, args.uncertainty, outdir)
+      histName = f'hist_{args.variable}_{args.mask}'
+      plot_unc(indir,histName, unc, outdir)
+      #plot_unc(indir,histName, args.uncertainty, outdir)
