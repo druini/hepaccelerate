1c736b63f6716d09ab0dd3cac6af15c2c6b25658
druini <daniele.ruini@cern.ch>
2020-11-18 17:56:44 +0100

    update plotting script for simpleFit

--- rhalphabet/makePlots.py
+++ rhalphabet/makePlots.py
@@ -19,6 +19,8 @@ rt.tdrStyle.SetPadLeftMargin(0.15)
 rt.tdrStyle.SetPadRightMargin(0.05)
 
 def makeOutdir(rootFilePath):
+  if not os.path.isfile(rootFilePath):
+      raise Exception(f'Cannot find file {rootFilePath}')
   if not rootFilePath.endswith(".root"):
     raise Exception(f'Must supply ROOT filename, but got {rootFilePath}')
   if not (os.path.sep in rootFilePath): #if the file is in the current directory
@@ -28,40 +30,59 @@ def makeOutdir(rootFilePath):
   #outdir = os.path.join(outdir,'plots')
   outdir = os.path.join(outdir,'plots'+os.path.splitext(rootFilePath.split('fitDiagnostics')[1])[0])
   if not os.path.exists(outdir):
-    os.mkdir(outdir)
+    os.makedirs(outdir)
   return outdir
 
-def TH1fromRooObj(RooObj, errorBand=None, returnHistArray=False):
+def TH1fromRooObj(RooObj, errorBand=None, extBins=None, returnBins=False, returnHistArray=False):
   if type(RooObj) == rt.RooHist:
     bins = range(RooObj.GetN())
   elif type(RooObj) == rt.RooCurve:
-    bins = range(RooObj.GetN())[3:-4:2]
+    if args.simpleFit:
+      bins = range(RooObj.GetN())[2:-4]
+    else:
+      bins = range(RooObj.GetN())[3:-4:2]
   else:
     raise Exception(f'unknown object type: {type(RooObj)}')
 
   hist = [[rt.Double(0),rt.Double(0)] for b in bins]
   for n,b in enumerate(bins):
     RooObj.GetPoint(b, *hist[n])
+  if extBins is not None:
+      hist = [b for b in hist if b[0] in extBins]
   binWidth = round(hist[1][0] - hist[0][0])
-  nbins    = len(bins)
+  nbins    = len(bins) if extBins is None else len(extBins)
 
   if returnHistArray: return np.array(hist)
 
   if errorBand is None:
     err = [sqrt(v[1]) for v in hist]
   else:
-    idxUp   = range(int(errorBand.GetN()/2))[3:-4:2]
-    idxDown = range(int(errorBand.GetN()/2),errorBand.GetN())[-4:3:-2]
-    assert( len(idxUp)==nbins )
-    assert( len(idxDown)==nbins )
-    errUp   = [[rt.Double(0),rt.Double(0)] for b in range(nbins)]
-    errDown = [[rt.Double(0),rt.Double(0)] for b in range(nbins)]
-    for n,i in enumerate(idxUp):
-      errorBand.GetPoint(i, *errUp[n])
-    for n,i in enumerate(idxDown):
-      errorBand.GetPoint(i, *errDown[n])
+      ###
+    if args.simpleFit:
+      nPoints = errorBand.GetN()
+      errUp   = [[rt.Double(0),rt.Double(0)] for b in range(2,int(nPoints/2)-1)]
+      errDown = [[rt.Double(0),rt.Double(0)] for b in range(2,int(nPoints/2)-1)]
+      for n,_ in enumerate(errUp):
+          errorBand.GetPoint(n, *errUp[n])
+          errorBand.GetPoint(nPoints-1-n, *errDown[n])
+      errUp   = [e for e in errUp if e[0] in np.array(hist)[:,0]]
+      errDown = [e for e in errDown if e[0] in np.array(hist)[:,0]]
+    else:
+      ###
+      idxUp   = range(int(errorBand.GetN()/2))[3:-4:2]
+      idxDown = range(int(errorBand.GetN()/2),errorBand.GetN())[-4:3:-2]
+      print(bins, idxUp, idxDown)
+      assert( len(idxUp)==nbins )
+      assert( len(idxDown)==nbins )
+      errUp   = [[rt.Double(0),rt.Double(0)] for b in range(nbins)]
+      errDown = [[rt.Double(0),rt.Double(0)] for b in range(nbins)]
+      for n,i in enumerate(idxUp):
+        errorBand.GetPoint(i, *errUp[n])
+      for n,i in enumerate(idxDown):
+        errorBand.GetPoint(i, *errDown[n])
+    for n,_ in enumerate(errUp):
       assert( errUp[n][0]-errDown[n][0]<.5 ) # check that up and down errors correspond to the same bin
-      assert( hist[n][1]-(errUp[n][1]+errDown[n][1])/2 < 1e-3 ) # check that histogram is in the middle of the error band
+      assert( (hist[n][1]-(errUp[n][1]+errDown[n][1])/2)/hist[n][1] < 1e-3 ) # check that histogram is in the middle of the error band
     err = [ (errUp[i][1] - errDown[i][1])/2 for i in range(nbins) ]
 
   if type(RooObj) == rt.RooHist:
@@ -74,6 +95,8 @@ def TH1fromRooObj(RooObj, errorBand=None, returnHistArray=False):
   for i in range(nbins):
     TH1.Fill( *hist[i] )
     TH1.SetBinError( i+1, err[i] ) # stupid root starts indexing at 1 instead of 0
+  if returnBins:
+    return TH1.Clone(), np.array(hist)[:,0]-binWidth/2
   return TH1.Clone()
 
 def plotRhalphaShapes(rootFilePath, nptbins):
@@ -109,8 +132,8 @@ def plotRhalphaShapes(rootFilePath, nptbins):
   for ibin in range(nptbins):
     for region in listRegion:
       for SorB in ['s' ]: #,'b']:
-        try:
-          p = f'pass_msd_fit_{SorB}' if args.simpleFit else f'ptbin{ibin}{region}_msd_fit_{SorB}'
+        #try:
+          p = f'boosted_ttH_msd_fit_{SorB}' if args.simpleFit else f'ptbin{ibin}{region}_msd_fit_{SorB}'
           can = rt.TCanvas('c2', 'c1',  10, 10, 750, 750 )
           pad1 = rt.TPad("pad1", "Fit",0,0.207,1.00,1.00,-1)
           pad2 = rt.TPad("pad2", "Pull",0,0.00,1.00,0.28,-1)
@@ -125,11 +148,11 @@ def plotRhalphaShapes(rootFilePath, nptbins):
           plot.Print()
           b_suffix = '_model_bonly__' if SorB=='b' else ''
           RooObj = {
-            'data'       : plot.getHist( 'h_pass' if args.simpleFit else f'h_ptbin{ibin}{region}'),
-            'errs'       : plot.getCurve( 'pdf_binpass_Norm[msd]_errorband' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_errorband'),
-            'signal'     : plot.getCurve( 'pdf_binpass_Norm[msd]_Comp[shapeSig*]' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeSig*]'),
-            'bkg'        : plot.getCurve( 'pdf_binpass_Norm[msd]_Comp[shapeBkg*]' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeBkg*]'),
-            'sigPlusBkg' : plot.getCurve( 'pdf_binpass_Norm[msd]' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]')
+            'data'       : plot.getHist( 'h_boosted_ttH' if args.simpleFit else f'h_ptbin{ibin}{region}'),
+            'errs'       : plot.getCurve( 'pdf_binboosted_ttH_Norm[msd]_errorband' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_errorband'),
+            'signal'     : plot.getCurve( 'pdf_binboosted_ttH_Norm[msd]_Comp[shapeSig*]' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeSig*]'),
+            'bkg'        : plot.getCurve( 'pdf_binboosted_ttH_Norm[msd]_Comp[shapeBkg*]' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]_Comp[shapeBkg*]'),
+            'sigPlusBkg' : plot.getCurve( 'pdf_binboosted_ttH_Norm[msd]' if args.simpleFit else f'pdf_binptbin{ibin}{region}_{b_suffix}Norm[msd]')
           }
 
           if args.isData:
@@ -167,8 +190,8 @@ def plotRhalphaShapes(rootFilePath, nptbins):
 
 
           hRatio = rt.TGraphAsymmErrors()
-          data_h = TH1fromRooObj(RooObj['data'])
-          sigPlusBkg_h = TH1fromRooObj(RooObj['bkg'],RooObj['errs'])
+          data_h, dataBins = TH1fromRooObj(RooObj['data'],returnBins=True)
+          sigPlusBkg_h = TH1fromRooObj(RooObj['bkg'],RooObj['errs'], extBins=dataBins)
           hRatio.Divide( data_h,sigPlusBkg_h, 'pois' )
           hRatioStatErr = sigPlusBkg_h.Clone()
           hRatioStatErr.Divide( sigPlusBkg_h )
@@ -200,7 +223,7 @@ def plotRhalphaShapes(rootFilePath, nptbins):
             print(ext)
             can.SaveAs( os.path.join(outdir,f'{p}.{ext}') )
           del can
-        except: continue
+        #except: continue
 
 def plotTF(rootFilePath, nptbins):
   outdir = makeOutdir(rootFilePath)
@@ -320,7 +343,7 @@ if __name__ == '__main__':
   elif args.year.endswith('2018'): args.lumi = 59740.
   else: args.lumi = 130000
   CMS_lumi.extraText = "Preliminary" if args.isData else "Simulation Preliminary"
-  CMS_lumi.lumi_13TeV = str( round( (args.lumi/1000.), 2 ) )+" fb^{-1}, "+args.year+" (13 TeV)"
+  CMS_lumi.lumi_13TeV = str( round( (args.lumi/1000.), 2 ) )+" fb^{-1}, "+(args.year+' ' if args.year.startswith('201') else '')+" (13 TeV)"
 
   if not args.simpleFit: plotTF(args.rootfile, args.nptbins)
   else: args.nptbins = 1
